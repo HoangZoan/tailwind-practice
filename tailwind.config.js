41522e0b81af3c8d9b40586e31a252d2ed4e38c4
HoangZoan <d.hoang1906@gmail.com>
2022-03-15 16:26:38 +0700

    Done

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,15 @@ module.exports = {
       fontFamily: {
         poppins: ["Poppins", "sans-serif"],
       },
+      container: {
+        center: true,
+        // padding: "1rem",
+        screens: {
+          lg: "1124px",
+          xl: "1124px",
+          "2xl": "1124px",
+        },
+      },
     },
   },
   plugins: [],
